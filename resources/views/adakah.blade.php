99977644dde3ab9e1d9ba8b99d6af7ab20ec92b1
MSI\andyr <60900119041@uin-alauddin.ac.id>
2021-11-09 11:37:24 +0800

    Pengertian VCS dan jenisjenisnya

---
+++ resources/views/adakah.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h3><b>Menurut Atlassian, version control system (VCS) adalah sebuah kumpulan perangkat lunak yang sudah terintegrasi dan digunakan untuk membantu software engineer mengelola perubahan dalam source code dari waktu ke waktu.</b></h3>
+    <h2>jenis-jenis vcs</h2>
+    <ul>Bazaar</ul>
+    <ul>Sub version</ul>
+    <ul>Mercurial</ul>
+    <ul>Revision Control System</ul>
+    <ul>Clear / Case</ul>
+</body>
+</html>
